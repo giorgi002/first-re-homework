18b62983f57cb6a91b68a93be5809d541f79ed93
giorgi002 <167451976+giorgi002@users.noreply.github.com>
2024-09-18 18:51:32 +0400

    Initial commit

---
+++ reed.js
@@ -0,0 +1,45 @@
+//first task;👇
+
+// function reversewords(string){
+//     return string.split(" ").reverse().join(" ")
+// }
+// let input = "Hello World";
+// console.log(reversewords(input));
+
+
+
+// second task 👇
+// function cleannumbers(sentence){
+//     return sentence.replace(/\d/g, "");
+// }
+// console.log(cleannumbers("this is5 great8"));
+
+
+
+// third task 👇
+
+// function multiplycharacters(string){
+//     return string.split("").map( char => char + char).join("");
+
+// }
+// console.log(multiplycharacters("hello"));
+
+
+
+// fourth task 👇
+
+// function getinitials(string){
+//      return string.split(" ").map(char => char[0].toUpperCase()).join(".");
+
+// }
+// console.log(getinitials("giorgi nadiradze"));
+
+
+
+// fifth task 👇
+
+// function managecharacter(length){
+//     const character =  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+//     return Array.from({length},() => character[Math.floor(Math.random() * character.length)]).join("");
+// }
+// console.log(managecharacter(4));
